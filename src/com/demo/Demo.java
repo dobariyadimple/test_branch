d2b297c645e52f530e085f4cbd01c1097f406f31
dimple_zaptech <dimple@zapserver.com>
2015-03-19 11:29:14 +0530

    chekout branch

---
+++ src/com/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Demo {
+	public static void main(String args[])
+	{
+		int a=23;
+		int b=43;
+		int c=a+b;
+		System.out.println("Sum of a and b is "+c);
+	}
+
+}
